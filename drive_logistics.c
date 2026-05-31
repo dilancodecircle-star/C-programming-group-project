205dd3595acb997f3a95e937af497bccaedaa686
Geethanjali Rathnayake <geethanjalirathnayake5@gmail.com>
2026-05-31 20:20:22 +0530

    add initial code for blood donation & matching system

---
+++ drive_logistics.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int choice;
+
+    while (1)
+    {
+        printf("\n======================================================\n");
+        printf(" Community Blood Donation & Matching System \n");
+        printf("======================================================\n");
+        printf("1. Donor Registry          \n");
+        printf("2. Inventory & Storage     \n");
+        printf("3. Testing & Screening     \n");
+        printf("4. Hospital Requests       \n");
+        printf("5. Event Logistics         \n");
+        printf("6. Exit System\n");
+        printf("Enter module number to access: ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            printf("\n--- Not Available ---\n");
+            break;
+        case 2:
+            printf("\n--- Not Available ---\n");
+            break;
+        case 3:
+            printf("\n--- Not Available ---\n");
+            break;
+        case 4:
+            printf("\n--- Not Available ---\n");
+            break;
+        case 5:
+        {
+            printf("\n--- Not Available ---\n");
+            break;
+        }
+        case 6:
+            printf("Exiting system. Goodbye!\n");
+            exit(0);
+        default:
+            printf("Invalid choice. Please try again.\n");
+        }
+    }
+    return 0;
+}
